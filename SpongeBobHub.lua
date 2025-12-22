4da3638e4841dd0c317564e39ce93b4abd1d12d6
lucasschauermusic-design <lucasschauermusic@gmail.com>
2025-12-22 12:36:35 +0100

    Update SpongeBobHub.lua

--- SpongeBobHub.lua
+++ SpongeBobHub.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "SpongeBob TD: Knit Scanner",
-   LoadingTitle = "Analysiere Controller-Methoden...",
+   LoadingTitle = "Analysiere Knit-Methoden...",
 })
 
 local MainTab = Window:CreateTab("Welten", 4483362458)
@@ -19,48 +19,32 @@ if MainTab then
     MainTab:CreateButton({
        Name = "Welt-Auswahl erzwingen",
        Callback = function()
-           print("!!! START: Knit-Methoden-Scan !!!")
+           print("!!! START: KNIT-METHODEN-SCAN !!!")
            
            local success, err = pcall(function()
+               -- Knit finden (basierend auf deinem Screenshot)
                local Knit = game:GetService("ReplicatedStorage"):FindFirstChild("Knit", true)
                local KnitClient = require(Knit.Parent.Knit)
                
+               -- Den Controller laden, den wir in deinem Scan gefunden haben
                local worldCtrl = KnitClient.GetController("FactionWorldController")
-               local uiCtrl = KnitClient.GetController("UIController")
 
-               -- SCAN 1: FactionWorldController
                if worldCtrl then
-                   print("--- Methoden in FactionWorldController: ---")
-                   for name, func in pairs(worldCtrl) do
-                       if type(func) == "function" then
-                           print("Gefunden: " .. name)
-                           -- Automatischer Versuch, wenn der Name passt
-                           if name:find("World") or name:find("Stage") or name:find("Select") then
-                               print("Versuche Aufruf: " .. name)
-                               pcall(function() worldCtrl[name](worldCtrl, selectedWorld) end)
-                           end
+                   print("--- Verfügbare Befehle in FactionWorldController: ---")
+                   -- Wir gehen alle Einträge im Controller durch
+                   for name, value in pairs(worldCtrl) do
+                       if type(value) == "function" then
+                           -- Wir schreiben jeden Funktionsnamen in den Log
+                           print("GEFUNDEN: " .. name)
                        end
                    end
-               end
-
-               task.wait(0.5)
-
-               -- SCAN 2: UIController (Hier liegt SelectChapter)
-               if uiCtrl then
-                   print("--- Methoden in UIController: ---")
-                   for name, func in pairs(uiCtrl) do
-                       if type(func) == "function" then
-                           print("Gefunden: " .. name)
-                           if name == "SelectChapter" then
-                               print("Knit: Aktiviere Kapitel 1 via UIController")
-                               uiCtrl:SelectChapter(1)
-                           end
-                       end
-                   end
+                   Rayfield:Notify({Title = "Scan fertig", Content = "Schau in das Codex-Log!"})
+               else
+                   print("Fehler: FactionWorldController nicht gefunden!")
                end
            end)
 
-           if not success then warn("Aktions-Fehler: " .. tostring(err)) end
+           if not success then warn("Scan-Fehler: " .. tostring(err)) end
        end,
     })
 end
