001eb2ab7cc60b1e0db8ed38905d0f1ce15f1b5a
lucasschauermusic-design <lucasschauermusic@gmail.com>
2025-12-30 12:51:53 +0100

    Update SpongeBobHub.lua

--- SpongeBobHub.lua
+++ SpongeBobHub.lua
@@ -1,1 +1,20 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "Spongebob Tower Defense",
+    Folder = "SpongebobTD",
+})
+
+local MainTab = Window:Tab({
+    Title = "Main",
+})
+
+local InfoSection = MainTab:Section({
+    Title = "Script Information",
+})
+
+InfoSection:Section({
+    Title = "Welcome to Spongebob Tower Defense Script",
+    TextSize = 16,
+    TextTransparency = 0.35,
+})
